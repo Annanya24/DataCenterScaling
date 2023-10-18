1a262f85aa4d9d8df197a1cce2095410baec7c3b
Annanya24 <41088161+Annanya24@users.noreply.github.com>
2023-10-17 22:48:39 -0600

    Add files via upload

---
+++ lab2/queries.sql
@@ -0,0 +1,46 @@
+SELECT td.animal_type, COUNT(distinct af.animal_dim_key) as animal_count
+FROM outcomes_fact af
+JOIN type_dim td ON af.type_id = td.type_id
+GROUP BY td.animal_type;
+
+
+select COUNT(*) as animals_with_multiple_outcomes
+FROM (
+    SELECT af.animal_dim_key
+    FROM outcomes_fact af
+    GROUP BY af.animal_dim_key
+    HAVING COUNT(*) > 1
+) 
+
+
+
+SELECT
+    month AS month_name,
+    COUNT(*) AS outcome_count
+FROM Date_dim
+JOIN outcomes_fact ON Date_dim.date_id = outcomes_fact.date_id
+GROUP BY month
+ORDER BY outcome_count DESC
+LIMIT 5;
+
+SELECT
+    CASE
+        WHEN EXTRACT(YEAR FROM AGE(outcomes.dob, outcomes.ts)) < 1 THEN 'Kitten'
+        WHEN EXTRACT(YEAR FROM AGE(outcomes.dob, outcomes.ts)) >= 1 AND EXTRACT(YEAR FROM AGE(outcomes.dob, outcomes.ts)) <= 10 THEN 'Adult'
+        WHEN EXTRACT(YEAR FROM AGE(outcomes.dob, outcomes.ts)) > 10 THEN 'Senior'
+    END AS age_category,
+    COUNT(*) AS count
+FROM outcomes
+WHERE outcomes.outcome_type = 'Adopted'
+GROUP BY age_category;
+
+
+SELECT
+    dd.ts AS date,
+    od.outcome_type,
+    SUM(COUNT(*)) OVER (PARTITION BY od.outcome_type ORDER BY dd.ts) AS cumulative_total
+FROM Date_dim AS dd
+JOIN outcomes_fact AS of ON dd.date_id = of.date_id
+JOIN outcome_dim AS od ON of.outcome_id = od.outcome_id
+GROUP BY dd.ts, od.outcome_type
+ORDER BY dd.ts, od.outcome_type;
